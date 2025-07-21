38bd77ad2d42323a1c6a33afe5dd13982cfbbcd4
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2025-07-21 21:07:08 +0330

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Nazmino',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // reload" button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
         //
-        // Notice that the counter didn't reset back to zero; the application
+        // Notice that the counter didn't reset backgit to zero; the application
         // state is not lost during the reload. To reset the state, use hot
         // restart instead.
         //
