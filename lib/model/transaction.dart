5d56650161b6da80110d7a18ab45474b444adb06
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2025-07-21 21:21:51 +0330

    implmnt tranaction provider

---
+++ lib/model/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final bool isInCome;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.isInCome,
+  });
+}
---
+++ lib/provider/transaction_provider.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:nazmino/model/transaction.dart';
+
+class TransactionProvider extends ChangeNotifier {
+  /// The list of transactions.
+  /// This is a private field that holds the transactions.
+  final List<Transaction> _transactions = [];
+
+  /// Get the list of transactions.
+  /// This is a getter that returns the transactions.
+  /// @return The list of transactions.
+  List<Transaction> get transactions => _transactions;
+
+  /// Add a transaction to the list.
+  /// This method adds a transaction to the list and notifies listeners.
+  /// @param transaction The transaction to add.
+  /// @return void
+  void addTransaction(Transaction transaction) {
+    _transactions.add(transaction);
+    notifyListeners();
+  }
+
+  /// Remove a transaction from the list.
+  /// This method removes a transaction from the list and notifies listeners.
+  /// @param transaction The transaction to remove.
+  /// @return void
+  void removeTransaction(Transaction transaction) {
+    _transactions.remove(transaction);
+    notifyListeners();
+  }
+
+  /// Clear all transactions from the list.
+  /// This method clears all transactions from the list and notifies listeners.
+  /// @return void
+  void clearTransactions() {
+    _transactions.clear();
+    notifyListeners();
+  }
+
+  /// Get the total amount of all transactions.
+  /// This method calculates the total amount of all transactions.
+  /// @return The total amount of all transactions.
+  double get totalAmount {
+    return _transactions.fold(0.0, (sum, transaction) {
+      return sum +
+          (transaction.isInCome ? transaction.amount : -transaction.amount);
+    });
+  }
+
+  /// Get the total income from all transactions.
+  /// This method calculates the total income from all transactions.
+  /// @return The total income from all transactions.
+  double get totalIncome {
+    return _transactions
+        .where((transaction) => transaction.isInCome)
+        .fold(0.0, (sum, transaction) => sum + transaction.amount);
+  }
+
+  /// Get the total expense from all transactions.
+  /// This method calculates the total expense from all transactions.
+  /// @return The total expense from all transactions.
+  double get totalExpense {
+    return _transactions
+        .where((transaction) => !transaction.isInCome)
+        .fold(0.0, (sum, transaction) => sum + transaction.amount);
+  }
+
+  /// Edit a transaction in the list.
+  /// This method edits a transaction in the list and notifies listeners.
+  /// @param oldTransaction The transaction to edit.
+  /// @param newTransaction The new transaction to replace the old one.
+  /// @return void
+  void editTransaction(Transaction oldTransaction, Transaction newTransaction) {
+    final index = _transactions.indexOf(oldTransaction);
+    if (index != -1) {
+      _transactions[index] = newTransaction;
+      notifyListeners();
+    }
+  }
+}
